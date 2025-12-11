49249c0dc1c846282e3f0d911b860ec0b93422b9
Aarav Singh <aaravsingh@Aaravs-MacBook-Air.local>
2025-12-11 15:15:19 +0530

    Main file changed

--- MAIN.C
+++ MAIN.C
@@ -2,7 +2,7 @@
 
 int main(){
     while(1){
-        printf("\n\t\t\t   1.Student panel \n\t\t\t   2.Admin panel \n\t\t\t   3.Exit \n\t\t\t   Option:");
+        printf(" cnahge kr rhe h \n\t\t\t      2.Admin panel \n\t\t\t   3.Exit \n\t\t\t   Option:");
 		char input;
         scanf(" %c",&input);
 
